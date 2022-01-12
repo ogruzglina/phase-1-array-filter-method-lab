70090ec8e0075b9c97a1a0f5f610762aa8d4ba01
Olha Gruzglina <olga.zhmura@gmail.com>
2022-01-12 18:03:44 -0500

    commit tree 4416f34d4d86b4210b8141242620c96d1dc9aa40

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const driversNames = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby', 'Sveta'];
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+console.log(findMatching(driversNames, 'Bobby'));
+console.log(findMatching(driversNames, 'bobby'));
+
+function fuzzyMatch(drivers, matchingWord) {
+    return drivers.filter( driver => driver.toLowerCase().indexOf(matchingWord.toLowerCase()) === 0);
+}
+console.log(fuzzyMatch(driversNames, 'sv'));
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
